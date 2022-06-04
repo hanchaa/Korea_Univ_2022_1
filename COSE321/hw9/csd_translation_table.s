f300154df57c597cb935d822f99f1fad297eeabd
Juhan <hanchaa@gmail.com>
2022-06-04 16:14:27 +0900

    chore(COSE_321_hw9): add some comments

--- COSE321/hw9/csd_translation_table.s
+++ COSE321/hw9/csd_translation_table.s
@@ -17,21 +17,22 @@ csd_MMU_table_fig2:
 .word	SECT + 0x15de6		/* S=b1 TEX=b101 AP=b11, Domain=b1111, C=b0, B=b1 */
 .word	csd_page_table_fig2 + 0x1e1		/* Domain=b1111 */
 
-.align 10
+
+.section .csd_mmu_tbl_lv2, "a"
 csd_page_table_fig1:
-.set PAGE, 0x400000
+.set PAGE, 0x400000    // 0x400000
 .word PAGE + 0x576 // S=b1 TEX=b101 AP=b11 C=b0 B=b1
-.set PAGE, PAGE+0x1000
+.set PAGE, PAGE+0x1000 // 0x401000
 .word PAGE + 0x576 // S=b1 TEX=b101 AP=b11 C=b0 B=b1
-.set PAGE, PAGE+0x1000
+.set PAGE, PAGE+0x1000 // 0x402000
 .word PAGE + 0x576 // S=b1 TEX=b101 AP=b11 C=b0 B=b1
 
 .align 10
 csd_page_table_fig2:
-.set PAGE, 0x400000
+.set PAGE, 0x400000    //0x400000
 .word PAGE + 0x576 // S=b1 TEX=b101 AP=b11 C=b0 B=b1
-.set PAGE, PAGE+0x2000
+.set PAGE, PAGE+0x2000 //0x402000
 .word PAGE + 0x576 // S=b1 TEX=b101 AP=b11 C=b0 B=b1
-.set PAGE, PAGE-0x2000
+.set PAGE, PAGE-0x2000 //0x400000
 .word PAGE + 0x576 // S=b1 TEX=b101 AP=b11 C=b0 B=b1
 .end
